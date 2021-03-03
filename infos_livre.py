a560aee1f45370895f9c6b185b95568036a8e61e
Omar Djait <djait.omar@gmail.com>
2021-03-02 22:18:02 -0500

    salut

--- infos_livre.py
+++ infos_livre.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from word2number import w2n
 import csv
+
 
 csv_columns = ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax",
                "number_available", "product_description", "category", "review_rating", "image_url"]
@@ -11,13 +13,13 @@ response = requests.get(url)  # code 200
 response.encoding = 'UTF-8'  # a réglé les problèmes d'exportation csv
 soup = BeautifulSoup(response.text, "lxml")  # affiche la page html
 
-UPC = soup.find("th", text="UPC").find_next("td").text
+upc = soup.find("th", text="UPC").find_next("td").text
 
 title = soup.find("h1").text
 
-inc_tax = soup.find("th", text="Price (incl. tax)").find_next("td").text[1:]
+inc_tax = soup.find("th", text="Price (incl. tax)").find_next("td").text
 
-exc_tax = soup.find("th", text="Price (excl. tax)").find_next("td").text[1:]
+exc_tax = soup.find("th", text="Price (excl. tax)").find_next("td").text
 
 num = soup.find("th", text="Availability").find_next("td").text
 num_m = (num.split()[2])[1:]
@@ -27,15 +29,17 @@ desc_m = desc.replace(" ...more", "")
 
 cat = soup.find("li", class_="active").find_previous("a").text
 
-rev = soup.find("p", class_="star-rating")["class"][1]  # plus d'explication
+rev = soup.find("p", class_="star-rating")["class"][1]
+rev_m = w2n.word_to_num(rev)
+print(rev_m)
 
 image_url = soup.find("div", class_="item active").find_next("img")["src"]
 image_url_m = image_url.replace("../..", "http://books.toscrape.com")
 
 infos = [
-    {"product_page_url": url, "universal_product_code": UPC, "title": title, "price_including_tax": inc_tax,
+    {"product_page_url": url, "universal_product_code": upc, "title": title, "price_including_tax": inc_tax,
      "price_excluding_tax": exc_tax, "number_available": num_m, "product_description": desc_m, "category": cat,
-     "review_rating": rev, "image_url": image_url_m}
+     "review_rating": rev_m, "image_url": image_url_m}
 ]
 
 print(infos)
--- livre_category.py
+++ livre_category.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 links = []
-for i in range(1, 5):
+
+i = 1
+url = "http://books.toscrape.com/catalogue/category/books/fiction_10/page-"+str(i)+".html"
+
+while str(requests.get(url)) == "<Response [200]>":
+
     print("Page num°:", i)
-
-    url = "http://books.toscrape.com/catalogue/category/books/fiction_10/page-"+str(i)+".html"
-
     response = requests.get(url)  # code 200
     response.encoding = 'UTF-8'  # a réglé les problèmes d'exportation csv
     soup = BeautifulSoup(response.text, "lxml")  # affiche la page html
@@ -15,6 +18,8 @@ for i in range(1, 5):
         liens = lien.a["href"]
         print(liens.replace("../../../", "http://books.toscrape.com/catalogue/"))
         links.append(liens.replace("../../../", "http://books.toscrape.com/catalogue/"))
+    i = i+1
+    url = "http://books.toscrape.com/catalogue/category/books/fiction_10/page-" + str(i) + ".html"
 
 with open("livre_category.csv", 'w') as file:
     for link in links:
